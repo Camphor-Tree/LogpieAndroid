753c90fd4016e6963b638c050fb4f097ef7bff43
yilei <zyl.echo@gmail.com>
2014-09-11 00:41:36 -0700

    Make LogpieActivity and LogpieLocation Parcelable, so that they can be passed in the intent

--- LogpieLib/src/com/logpie/android/logic/LogpieActivity.java
+++ LogpieLib/src/com/logpie/android/logic/LogpieActivity.java
@@ -2,14 +2,36 @@ package com.logpie.android.logic;
 
 import java.util.List;
 
-public class LogpieActivity
+import android.os.Parcel;
+import android.os.Parcelable;
+
+//TODO: remove all the set method. All the member variable should be final.
+/**
+ * Make LogpieActivity parcelable, so that it can be pass in the intent.
+ * 
+ * @author yilei
+ * 
+ */
+public class LogpieActivity implements Parcelable
 {
     private String mID;
     private String mDescription;
-    private LogpieLocation Location;
+    private LogpieLocation mLocation;
     // TODO combine the time into one Period class
-    private String startTime;
-    private String endTime;
+    private String mStartTime;
+    private String mEndTime;
+    private int mCountLike;
+
+    public int getmCountLike()
+    {
+        return mCountLike;
+    }
+
+    public void setmCountLike(int mCountLike)
+    {
+        this.mCountLike = mCountLike;
+    }
+
     private List<Comment> mComments;
 
     public String getID()
@@ -34,32 +56,32 @@ public class LogpieActivity
 
     public LogpieLocation getLocation()
     {
-        return Location;
+        return mLocation;
     }
 
     public void setLocation(LogpieLocation location)
     {
-        Location = location;
+        mLocation = location;
     }
 
     public String getStartTime()
     {
-        return startTime;
+        return mStartTime;
     }
 
     public void setStartTime(String startTime)
     {
-        this.startTime = startTime;
+        this.mStartTime = startTime;
     }
 
     public String getEndTime()
     {
-        return endTime;
+        return mEndTime;
     }
 
     public void setEndTime(String endTime)
     {
-        this.endTime = endTime;
+        this.mEndTime = endTime;
     }
 
     public List<Comment> getComments()
@@ -77,11 +99,53 @@ public class LogpieActivity
     {
         mID = id;
         mDescription = description;
-        Location = location;
+        mLocation = location;
         // TODO combine the time into one Period clss
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.mStartTime = startTime;
+        this.mEndTime = endTime;
         mComments = comments;
+    }
+
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<LogpieActivity> CREATOR = new Parcelable.Creator<LogpieActivity>()
+    {
+        public LogpieActivity createFromParcel(Parcel in)
+        {
+            return new LogpieActivity(in);
+        }
+
+        public LogpieActivity[] newArray(int size)
+        {
+            return new LogpieActivity[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeString(mID);
+        dest.writeString(mDescription);
+        dest.writeString(mStartTime);
+        dest.writeString(mEndTime);
+        dest.writeInt(mCountLike);
+        dest.writeParcelable(mLocation, flags);
+
+    }
+
+    private LogpieActivity(Parcel in)
+    {
+        mID = in.readString();
+        mDescription = in.readString();
+        mStartTime = in.readString();
+        mEndTime = in.readString();
+        mCountLike = in.readInt();
+        mLocation = in.readParcelable(getClass().getClassLoader());
+
     }
 
 }
--- LogpieLib/src/com/logpie/android/logic/LogpieLocation.java
+++ LogpieLib/src/com/logpie/android/logic/LogpieLocation.java
@@ -1,11 +1,20 @@
 package com.logpie.android.logic;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.logpie.android.gis.GisAPIHelper;
 import com.logpie.android.util.LogpieLog;
 
-public class LogpieLocation
+/**
+ * Make LogpieLocation parcelable, so that it can be pass in the intent.
+ * 
+ * @author yilei
+ * 
+ */
+public class LogpieLocation implements Parcelable
 {
-	private static String TAG = LogpieLocation.class.getName();
+    private static String TAG = LogpieLocation.class.getName();
     private Double mLatitude;
     private Double mLongitude;
     private String mAddress;
@@ -20,10 +29,10 @@ public class LogpieLocation
     {
         this(null, null, address, null);
     }
-    
+
     public LogpieLocation(Double lat, Double lon, String address)
     {
-        this(lat,lon,address,null);
+        this(lat, lon, address, null);
     }
 
     public LogpieLocation(Double lat, Double lon, String address, String city)
@@ -38,11 +47,12 @@ public class LogpieLocation
     {
         mCity = city;
     }
-    
+
     public String getCity()
     {
         return mCity;
     }
+
     /**
      * @return the latitude
      */
@@ -96,14 +106,50 @@ public class LogpieLocation
 
     public String getCurrentCity()
     {
-        if(mLatitude!=null&&mLongitude!=null)
+        if (mLatitude != null && mLongitude != null)
         {
-        	LogpieLog.d(TAG, "Output latitude & longitude: " + mLatitude + ", " + "mLongitude");
+            LogpieLog.d(TAG, "Output latitude & longitude: " + mLatitude + ", " + "mLongitude");
             return GisAPIHelper.getCityFromLatLon(mLatitude, mLongitude);
         }
         else
         {
             return null;
         }
+    }
+
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<LogpieLocation> CREATOR = new Parcelable.Creator<LogpieLocation>()
+    {
+        public LogpieLocation createFromParcel(Parcel in)
+        {
+            return new LogpieLocation(in);
+        }
+
+        public LogpieLocation[] newArray(int size)
+        {
+            return new LogpieLocation[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeDouble(mLatitude.doubleValue());
+        dest.writeDouble(mLongitude.doubleValue());
+        dest.writeString(mAddress);
+        dest.writeString(mCity);
+    }
+
+    private LogpieLocation(Parcel in)
+    {
+        mLatitude = Double.valueOf(in.readDouble());
+        mLongitude = Double.valueOf(in.readDouble());
+        mAddress = in.readString();
+        mCity = in.readString();
     }
 }
